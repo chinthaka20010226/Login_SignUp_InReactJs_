ec4f593ed17df9327b75df5aafe12d7d94a675e0
chinthaka20010226 <prabathchinthaka361@gmail.com>
2024-01-25 02:21:37 +0530

    create file Structure...

---
+++ src/Components/LoginSignUp/LoginSignUp.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './LoginSignUp.css';
+
+import user_icon from '../Assets/person.png';
+import email_icon from '../Assets/email.png';
+import password_icon from '../Assets/password.png';
+
+function LoginSignUp(){
+    return(
+        <div></div>
+    )
+}
+
+export default LoginSignUp;
