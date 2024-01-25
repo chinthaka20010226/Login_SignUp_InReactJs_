0a7274e2b5b1de8b820a573863bd44b9c95b702c
chinthaka20010226 <prabathchinthaka361@gmail.com>
2024-01-25 22:37:42 +0530

    html code in LoginSignUp Coponent...

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 import LoginSignUp from './Components/LoginSignUp/LoginSignUp';
 
--- src/Components/LoginSignUp/LoginSignUp.js
+++ src/Components/LoginSignUp/LoginSignUp.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './LoginSignUp.css';
 
-import user_icon from '../Assets/person.png';
+import name_icon from '../Assets/person.png';
 import email_icon from '../Assets/email.png';
 import password_icon from '../Assets/password.png';
 
