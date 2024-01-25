ced0783cb463385c96766e3eb0733d05e48a7fc1
chinthaka20010226 <prabathchinthaka361@gmail.com>
2024-01-25 23:07:07 +0530

    Now, Structure is done...

--- src/Components/LoginSignUp/LoginSignUp.js
+++ src/Components/LoginSignUp/LoginSignUp.js
@@ -7,7 +7,33 @@ import password_icon from '../Assets/password.png';
 
 function LoginSignUp(){
     return(
-        <div></div>
+        <div className='container'>
+            <div className='header'>
+                <div className='header-text'>Login</div>
+                <div className='header-underline'></div>
+            </div>
+            <div className='inputs'>
+                <div className='input'>
+                    <img src={name_icon} alt='input-name'></img>
+                    <input type='text' placeholder='Name'></input>
+                </div>
+                <div className='input'>
+                    <img src={email_icon} alt='input-email'></img>
+                    <input type='email' placeholder='Email'></input>
+                </div>
+                <div className='input'>
+                    <img src={password_icon} alt='input-password'></img>
+                    <input type='password' placeholder='Password'></input>
+                </div>
+            </div>
+            <div className='forgot-password'>
+                Lost Password? <span>Click Here!</span>
+            </div>
+            <div className='submit-container'>
+                <div className='submit'>Sign Up</div>
+                <div className='submit'>Login</div>
+            </div>
+        </div>
     )
 }
 
